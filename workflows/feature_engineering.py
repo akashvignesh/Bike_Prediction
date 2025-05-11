e68b45ef6c5be030cbfb9c3d577dab629eda1578
akashvignesh <s3akash@gmail.com>
2025-05-11 10:47:01 -0400

    ffdsdsss

--- workflows/feature_engineering.py
+++ workflows/feature_engineering.py
@@ -193,7 +193,7 @@ feature_group = feature_store.get_or_create_feature_group(
     primary_key=["location_id", "pickup_hour"],
     event_time="pickup_hour"
 )
-df_transformed = df_transformed.drop(columns=["is_weekend"])
+df_transformed["is_weekend"] = df_transformed["is_weekend"].astype(int)
 logger.info(df_transformed)
 feature_group.insert(df_transformed, write_options={"wait_for_job": False})
 logger.info("✅ Data uploaded to Hopsworks")
